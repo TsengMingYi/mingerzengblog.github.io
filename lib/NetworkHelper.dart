755cecc271d2cfc6d934e33c52bb39ab421f3fa4
mingerzeng <www.mingerzeng@gmail.com>
2022-11-28 16:26:36 +0800

    hello1

--- lib/NetworkHelper.dart
+++ lib/NetworkHelper.dart
@@ -11,8 +11,8 @@ import 'models/Product.dart';
     http.Response response = await http.get(Uri.parse(apiUrl));
     if(response.statusCode == 200){
       String data = utf8decoder.convert(response.bodyBytes);
-      int total = jsonDecode(data)['total'];
-      for(int i = 0;i<total;i++){
+      int limit = jsonDecode(data)['limit'];
+      for(int i = 0;i<limit;i++){
         Blog blog = Blog();
         int blogId = jsonDecode(data)[i]['blogId'];
         String blogName = jsonDecode(data)[i]['blogName'];
